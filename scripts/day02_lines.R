c76d75931bc8ba9be4b2593fe70c1886cf62bf78
loreabad6 <lore.abad6@gmail.com>
2021-11-02 21:19:02 +0100

    Day 2

--- scripts/day02_lines.R
+++ scripts/day02_lines.R
@@ -6,23 +6,31 @@ library(here)
 extrafont::loadfonts(device = "win")
 
 ec = ne_countries(scale = "medium", country = "Ecuador", returnclass = "sf")
-roads = read_sf(here("data/igm/vias.shp"))
+rail = read_sf(here("data/igm/ferrocarril.shp"))
+road = read_sf(here("data/igm/vias.shp"))
 
 bbox_cont = ec %>% 
   st_cast("POLYGON") %>% 
   mutate(area = st_area(geometry)) %>% 
   top_n(1) %>% 
   st_bbox()
-
-tm_shape(roads, bbox = bbox_cont) +
+# 
+# tm_shape(ec, bbox = bbox_cont) +
+#   tm_fill(col = "#060606") +
+  tm_shape(road, bbox = bbox_cont) +
+  tm_lines(
+    col = "grey70",
+    alpha = 0.1, 
+    size = 0.001
+  ) +
+  tm_shape(rail) +
   tm_lines(
     col = "white",
-    size = 0.01, 
     alpha = 0.75
   ) +
   tm_layout(bg.color = "black") +
   tm_credits(
-    "Road network in continental Ecuador \n #30DayMapChallenge | Day 2: Lines \n Data from IGM | Created by @loreabad6",
+    "Ferrocarril al Cielo: Ecuadorian Railway \n #30DayMapChallenge | Day 2: Lines \n Data from IGM | Created by @loreabad6",
     col = "white", align = "right", position = "right", fontfamily = "Futura LT"
   )
 
