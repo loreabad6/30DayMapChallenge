9b1a868f218dd5668165ff33846a6ef876658f82
loreabad6 <lore.abad6@gmail.com>
2021-11-02 22:11:01 +0100

    Day 3

---
+++ scripts/day03_polygons.R
@@ -0,0 +1,44 @@
+library(sf)
+library(rnaturalearth)
+library(tidyverse)
+library(tmap)
+library(here)
+library(osmdata)
+extrafont::loadfonts(device = "win")
+
+ec = ne_countries(scale = "medium", country = "Ecuador", returnclass = "sf")
+
+bbox_cont = ec %>% 
+  st_cast("POLYGON") %>% 
+  mutate(area = st_area(geometry)) %>% 
+  top_n(1) %>% 
+  st_bbox()
+
+reserves = opq(bbox = bbox_cont) %>% 
+  add_osm_feature(key = 'leisure', value = "nature_reserve", value_exact = FALSE) %>%
+  osmdata_sf() 
+
+reserves_uq = reserves %>% 
+  unique_osmdata() %>% 
+  unname_osmdata_sf()
+
+reserves_multi = reserves_uq$osm_multipolygons %>% 
+  st_cast("POLYGON")
+reserves_poly = reserves_uq$osm_polygons %>% 
+  bind_rows(reserves_multi) %>% 
+  filter(leisure == "nature_reserve")
+  
+tm_shape(ec, bbox = bbox_cont) +
+  tm_fill(col = "#090909") +
+  tm_shape(reserves_multi, bbox = bbox_cont) +
+  tm_fill(
+    col = "white",
+    alpha = 0.2
+  ) +
+  tm_layout(bg.color = "black") +
+  tm_credits(
+    "SNAP: National System of Protected Areas \n #30DayMapChallenge | Day 3: Polygons \n Data: OpenStreetMap | Created by @loreabad6",
+    col = "white", align = "right", position = "right", fontfamily = "Futura LT"
+  )
+
+tmap_save(filename = "maps/day03.png", bg = "black")
