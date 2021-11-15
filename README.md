11dde9f1a882d992c68a69c07ae5001a96a63a6c
loreabad6 <lore.abad6@gmail.com>
2021-11-15 18:04:35 +0100

    Day 14

--- README.md
+++ README.md
@@ -13,5 +13,6 @@
   <img src="maps/day10.png" width="260" height="260" /> 
   <img src="maps/day11.png" width="390" height="260" /> 
   <img src="maps/day12.png" width="240" height="260" /> 
-  <img src="maps/day13.png" width="360" height="260" /> 
+  <img src="maps/day13.png" width="300" height="260" /> 
+  <img src="maps/day14.png" width="240" height="260" /> 
 </p>
---
+++ scripts/day14_newtool.R
@@ -0,0 +1,32 @@
+library(roughsf)
+library(tidyverse)
+library(randomcoloR)
+library(sf)
+extrafont::loadfonts(device = "win")
+
+# https://data.humdata.org/dataset/ecuador-admin-level-2-boundaries
+cantones = read_sf(
+  "data/humdata/ecu_adm_inec_20190724_shp/ecu_admbnda_adm2_inec_20190724.shp"
+)
+
+cantones_rough = cantones %>% 
+  filter(ADM1_ES == "Guayas") %>% 
+  arrange(Shape_Area) %>% 
+  mutate(
+    fill = randomColor(25),
+    fillweight = 0.7,
+    stroke = 0.5,
+    hachureangle = runif(25, min = 0, max = 360)
+  ) %>% 
+  st_cast("POLYGON")
+
+map = roughsf(
+  cantones_rough, simplification = 2,
+  title = "Guayas Province", 
+  caption = "#30DayMapChallenge | Day 14: Map with a new tool | Data: INEC | Created by @loreabad6",
+  title_font = "50px Dancing Script",
+  caption_font = "25px Dancing Script",
+  width = 1000, height = 1000
+)
+
+save_roughsf(map, file = "maps/day14.png")
